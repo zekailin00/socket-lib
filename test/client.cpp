1f9a4b9091cb183df96c1eeef9481a78367c0ee9
Richard Yan <yrh@berkeley.edu>
2023-10-12 15:38:19 -0700

    socket library

---
+++ test/client.cpp
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <iomanip>
+#include <iostream>
+#include <stdint.h>
+#include <socket_lib.h>
+#include <unistd.h>
+
+struct test_args {
+  uint32_t addr;
+  uint32_t size;
+};
+
+void printVectorInHex(const std::vector<char>& buf) {
+    for (const char& c : buf) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c) << " ";
+    }
+    std::cout << std::dec << std::endl;
+}
+
+int main() {
+  init_client(6969);
+  std::vector<char> buf;
+  while (true) {
+    sleep(1);
+    if (socket_receive(69, false, buf)) {
+      struct test_args args;
+      deserialize_args(buf, args);
+      std::cout << "addr=0x" << std::hex << args.addr << ", size=0x" << args.size << std::endl;
+      printVectorInHex(buf);
+    }
+    if (socket_receive(420, false, buf)) {
+      std::cout << "received blank call" << std::endl;
+    }
+  }
+  return 0;
+}
---
+++ test/server.cpp
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <iomanip>
+#include <iostream>
+#include <stdint.h>
+#include <socket_lib.h>
+#include <unistd.h>
+
+struct test_args {
+  uint32_t addr;
+  uint32_t size;
+};
+
+int main() {
+  char tosend_arr[4] = {(char) 0xdeu, (char) 0xadu, (char) 0xbeu, (char) 0xefu};
+  std::vector<char> tosend;
+  tosend.push_back((char)0xde);
+  tosend.push_back((char)0xde);
+  tosend.push_back((char)0xde);
+  tosend.push_back((char)0xde);
+  struct test_args tosend_args;
+  tosend_args.addr = 0x8badf00dU;
+  tosend_args.size = 0x12345678U;
+
+  init_server(6969);
+  std::vector<char> args_buf;
+  args_buf = serialize_args(tosend_args);
+  std::vector<char> buf;
+  while (true) {
+    sleep(1);
+    socket_send(0, 69, args_buf, tosend);
+    socket_send(0, 420, empty_vec, empty_vec);
+  }
+  return 0;
+}
